6a3e4fe10603107f5e0c1a624a32d3030f3b7b6b
Jake 'Sid' Smith <jake@dabapps.com>
2017-10-22 20:34:49 +0100

    Test actions with enzyme

--- tests/index.tsx
+++ tests/index.tsx
@@ -1,3 +1,5 @@
+const dispatchMock = jest.fn();
+
 const mockApp = () => {
   // Require the un-mocked module to prevent infinite loop
   const original = require.requireActual('../src/ts/app');
@@ -7,16 +9,23 @@ const mockApp = () => {
   return {
     ...original,
     // Prepropulate our mock
-    default: (props: any) => <OriginalApp {...props} count={5} />
+    default: (props: any) => <OriginalApp {...props} count={5} dispatch={dispatchMock} />
   };
 };
 
 // We have to keep our mock function outside of this context so jest doesn't complain
 jest.mock('../src/ts/app', () => mockApp());
 
+import * as Enzyme from 'enzyme';
+import { mount } from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
 import App from '../src/ts/app';
+
+Enzyme.configure({
+  adapter: new Adapter()
+});
 
 describe('App', () => {
 
@@ -26,6 +35,16 @@ describe('App', () => {
     );
 
     expect(tree).toMatchSnapshot();
-  })
+  });
+
+  it('should trigger the increment action', () => {
+    const wrapper = mount(<App />);
+
+    expect(dispatchMock).not.toHaveBeenCalled();
+
+    wrapper.find('button').simulate('click');
+
+    expect(dispatchMock).toHaveBeenCalledWith({type: 'INCREMENT'});
+  });
 
 });
