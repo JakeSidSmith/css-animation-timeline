d8bd28ec3bad2e40d5edfb0f6190d45ca6ef03a7
Jake 'Sid' Smith <jake@dabapps.com>
2017-10-23 01:41:46 +0100

    Create global only type

--- types/global/index.d.ts
+++ types/global/index.d.ts
@@ -1,15 +1,19 @@
 declare type Diff<T extends string, U extends string> = ({
-  [P in T]: P
+  [P in T]: P;
 } & {
-  [P in U]: never
+  [P in U]: never;
 } & {
-  [x: string]: never
+  [x: string]: never;
 })[T];
 
 declare type Omit<T, K extends keyof T> = {
-  [P in Diff<keyof T, K>]: T[P]
+  [P in Diff<keyof T, K>]: T[P];
 };
 
 declare type Overwrite<T, U> = {
-  [P in Diff<keyof T, keyof U>]: T[P]
+  [P in Diff<keyof T, keyof U>]: T[P];
 } & U;
+
+declare type Only<T, K extends keyof T> = {
+  [P in K]: T[K];
+}
