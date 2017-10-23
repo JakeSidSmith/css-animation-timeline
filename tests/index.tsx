54ddd07721ac00933d68abea84bb7c91c71a22c1
Jake 'Sid' Smith <jake@dabapps.com>
2017-10-23 20:50:25 +0100

    Update test

--- tests/index.tsx
+++ tests/index.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
+import { Provider } from 'react-redux';
 import * as renderer from 'react-test-renderer';
 import App from '../src/ts/app';
+import { store } from '../src/ts/store';
 
 describe('App', () => {
 
   it('should match snapshot', () => {
     const tree = renderer.create(
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     );
 
     expect(tree).toMatchSnapshot();
